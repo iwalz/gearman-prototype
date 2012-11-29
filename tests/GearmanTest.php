2093045d43069c4d904c96ddd4aedb580bbedddf
ingo <ingo.walz@googlemail.com>
2012-11-30 00:42:17 +0100

    foo

--- tests/GearmanTest.php
+++ tests/GearmanTest.php
@@ -10,11 +10,11 @@ class GearmanTest extends PHPUnit_Framework_TestCase
     public function testReverseFunction() 
     {
         $client= new GearmanClient();
-        $ret = $client->addServer('localhost', 4730);
+        $ret = $client->addServer('127.0.0.1', 4730);
         if($ret) {
             echo "Connected" .  PHP_EOL;
         }
-        $val = $client->doNormal("reverse", "Hello World!");
+        $val = $client->do("reverse", "Hello World!");
         $this->assertEquals("!dlroW olleH", $val);
     }
 }
